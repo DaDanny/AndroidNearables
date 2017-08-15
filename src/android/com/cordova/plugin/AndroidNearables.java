f84fff79164376cc218b225daf20b2bdf5fc808d
Danny <danfrancken@gmail.com>
2017-08-14 18:41:40 -0700

    Updating master

--- src/android/com/cordova/plugin/AndroidNearables.java
+++ src/android/com/cordova/plugin/AndroidNearables.java
@@ -7,17 +7,28 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.content.Context;
 import android.widget.Toast;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Nearable;
+import java.util.List;
+import android.util.Log;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class AndroidNearables extends CordovaPlugin {
+    private BeaconManager beaconManager;
+    private List nearableList;
+    public Timer timer = new Timer();
+
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("getNearables")) {
+            beaconManager = new BeaconManager(webView.getContext());
+            this.getNearables(callbackContext);
             return true;
         }
         return false;
@@ -34,5 +45,39 @@ public class AndroidNearables extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    private void getNearables(final CallbackContext callbackContext) {
+        final JSONArray nearableArray = new JSONArray();
+
+        beaconManager.setNearableListener(new BeaconManager.NearableListener() {
+            @Override public void onNearablesDiscovered(List<Nearable> nearables) {
+                for(Nearable nearable : nearables) {
+                    try {
+                        JSONObject nearableObj = new JSONObject();
+                        nearableObj.put("identifier", nearable.identifier);
+                        if(!nearableArray.toString().contains("\"identifier\":\""+nearable.identifier+"\"")) {
+                            nearableArray.put(nearableObj);
+                        }
+                    } catch(JSONException e) {
+                        Log.e("AndroidNearables", "Invalid JSON String: " + e);
+                    }
+                }
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        beaconManager.disconnect();
+                        callbackContext.success(nearableArray);
+                    }
+                }, 4000);
+
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+                beaconManager.startNearableDiscovery();
+            }
+        });
     }
 }
--- www/AndroidNearables.js
+++ www/AndroidNearables.js
@@ -1,14 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "AndroidNearables", "coolMethod", [arg0]);
+exports.getNearables = function(arg0, success, error) {
+    exec(success, error, "AndroidNearables", "getNearables", [arg0]);
 };
-
-exports.echo = function(arg0, success, error) {
-    exec(success, error, 'AndroidNearables', 'echo', [arg0]);
-}
-
-exports.echoText = function(arg0, success, error) {
-    console.log('arg0: ', arg0);
-    success();
-}
